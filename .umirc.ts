ea62133c69daccffb585f0f01573edc632f6f7a8
z <1446433486@qq.com>
2022-10-18 11:13:49 +0800

    docs

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+  outputPath: 'docs/note'
   // more config: https://d.umijs.org/config
 });
